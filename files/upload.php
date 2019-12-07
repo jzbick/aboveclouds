2ec442a584b4de3d2b6c30f72bf0699afaebe9d9
Jonathan Zbick <jonathan.zbick@newcubator.com>
2019-12-07 17:10:56 +0100

    implement upload

--- files/upload.php
+++ files/upload.php
@@ -1,4 +1,16 @@
-<form method="post" action="upload.php">
-    <input type="file" name="userfile">
-    <input type="submit" value="Send File">
-</form>
+<?php
+   if($_COOKIE['N_ID']){ 
+        $uploaddir = '../userfiles/' . $_COOKIE['N_ID'] . '/';
+        $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+        if (!file_exists($uploaddir)) {
+            mkdir($uploaddir, 0777, true);
+        }
+        move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile);
+    }else {
+        header('Location: http://aboveclouds.online/');
+    }
+?>
+
+<script> 
+    window.history.back();
+</script>
