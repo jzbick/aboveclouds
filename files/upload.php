6445abfe53c020fdef562314d0cd74f6e553448f
Jonathan Zbick <jonathan.zbick@newcubator.com>
2019-12-11 15:47:05 +0100

    add newfile

--- files/upload.php
+++ files/upload.php
@@ -1,11 +1,17 @@
 <?php
-   if($_COOKIE['N_ID']){ 
+    if($_COOKIE['N_ID']){ 
         $uploaddir = '../userfiles/' . $_COOKIE['N_ID'] . '/';
         $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+        $file_name = $_FILES['userfile']['name'];
+        $file_path = $uploadfile;
+        $file_adatum = date('Y-m-d H:i:s');
+        $file_nid = $_COOKIE['N_ID'];
         if (!file_exists($uploaddir)) {
             mkdir($uploaddir, 0777, true);
         }
         move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile);
+        include_once './newFile.php';
+        $new_file->execute();
     }else {
         header('Location: http://aboveclouds.online/');
     }
