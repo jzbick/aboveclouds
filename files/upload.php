f76270f988c3a7726035122a5145bab39a254e02
Jonathan Zbick <jonathan.zbick@newcubator.com>
2019-12-17 12:47:27 +0100

    implement multiple upload

--- files/upload.php
+++ files/upload.php
@@ -1,24 +1,22 @@
 <?php
-    if($_COOKIE['N_ID']){ 
+if ($_COOKIE['N_ID']) {
+    for ($i = 0; $i < count($_FILES['userfile']['name']); $i++) {
         $uploaddir = '../userfiles/' . $_COOKIE['N_ID'] . '/';
-        $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-        $file_name = $_FILES['userfile']['name'];
+        $uploadfile = $uploaddir . basename($_FILES['userfile']['name'][$i]);
+        $file_name = $_FILES['userfile']['name'][$i];
         $file_path = $uploadfile;
         $file_adatum = date('Y-m-d H:i:s');
         $file_nid = $_COOKIE['N_ID'];
         if (!file_exists($uploaddir)) {
             mkdir($uploaddir, 0777, true);
         }
-        if(!file_exists($uploadfile)){
-            move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile);
+        if (!file_exists($uploadfile)) {
+            move_uploaded_file($_FILES['userfile']['tmp_name'][$i], $uploadfile);
             include_once './newFile.php';
             $new_file->execute();
         }
-    }else {
-        header('Location: http://aboveclouds.online/');
     }
-?>
-
-<script> 
-    window.location.pathname= '/aboveclouds/main/main.php';
-</script>
+} else {
+    header('Location: http://aboveclouds.online/');
+}
+header('Location: ../main/main.php');
