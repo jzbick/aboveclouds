fafd60c955182c497b314160c6d9250bd7e686a3
K728 <57729921+k728@users.noreply.github.com>
2019-12-18 18:04:54 +0100

    add checkPWD DB Statement

---
+++ main/templates/checkPWD.php
@@ -0,0 +1,11 @@
+<?php
+/* prepared Statement used to verify login */
+include __DIR__ . '/../config/datenbankzugriff.php';
+
+$sql_pwd = 'SELECT Passwort FROM nutzer WHERE Email = :mail';
+
+/* Im Hauptprogramm müssen vor dem Include Befehl die nachfolgenden Variablen angelegt worden sein */
+$getCheckValue = $dbc->prepare($sql_pwd);
+$getCheckValue->bindParam(':mail', $var_mail);
+
+?>
