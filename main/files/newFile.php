7b56bdc6519a84045fb0c2c6fb9db3f2a5b0b742
Jonathan Zbick <jonathan.zbick@newcubator.com>
2020-01-17 12:47:32 +0100

    Revert "add TYp"
    This reverts commit 7ab5452b3c0049dc956ee06e11853542158c450d.

--- main/files/newFile.php
+++ main/files/newFile.php
@@ -1,11 +1,10 @@
 <?php
 include_once '../config/config.php';
 
-$sql = 'INSERT INTO Datei (Name,Pfad,Typ,ADatum,N_ID) VALUES (:name,:pfad,:typ,:adatum,:nid)';
+$sql = 'INSERT INTO Datei (Name,Pfad,ADatum,N_ID) VALUES (:name,:pfad,:adatum,:nid)';
 
 $new_file = $dbc->prepare($sql);
 $new_file->bindParam(':name', $file_name);
 $new_file->bindParam(':pfad', $file_path);
-$new_file->bindParam(':typ', 'file');
 $new_file->bindParam(':adatum', $file_adatum);
 $new_file->bindParam(':nid', $file_nid);
