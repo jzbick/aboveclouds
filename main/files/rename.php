d1b28d4c700efa0da242b6395b93a7e555333c0c
Jonathan Zbick <jonathan.zbick@newcubator.com>
2020-01-19 15:16:57 +0100

    add rename

---
+++ main/files/rename.php
@@ -0,0 +1,16 @@
+<?php
+include_once '../config/config.php';
+include './getPath.php';
+include './getName.php';
+
+$file_path = getPath($_GET['did'], $dbc);
+$file_name = getName($_GET['did'], $dbc);
+$new_name = $_GET['new_name'];
+if (file_exists($file_path)) {
+    $new_path =  str_replace($file_name, $new_name, $file_path);
+    rename($file_path, $new_path) or die('File could not be renamed.');
+    include './renameFile.php';
+    $rename_file->execute();
+}
+
+header('Location: ../main.php');
