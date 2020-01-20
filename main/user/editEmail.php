b2cb6468b7017707001f815231a9b83a3d60fe53
Jonathan Zbick <jonathan.zbick@newcubator.com>
2020-01-20 21:44:38 +0100

    add edit email

---
+++ main/user/editEmail.php
@@ -0,0 +1,14 @@
+<?php
+include_once '../config/config.php';
+
+$sql = "update nutzer set email=:email where n_id=:nid";
+
+$new_email = $_PUT['new_email'];
+if (isset($new_email, $_SESSION['N_ID'])) {
+    $edit_email = $dbc->prepare($sql);
+    $edit_email->bindParam(':email', $new_email);
+    $edit_email->bindParam(':nid', $_SESSION['N_ID']);
+    $edit_email->execute();
+}
+
+header('Location: ../main.php');
